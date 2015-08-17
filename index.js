f19dc16ab6a84f4c854a22c5bd65e3062f9dac0a
Will Monk <will@williammonk.co.uk>
2015-08-17 16:42:03 +0100

    Updated toString function

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var Promise = require('bluebird');
 var postcss = require('postcss');
 var fs = require('fs');
 var _ = require('lodash');
+var colors = require('colors');
 
 function makeId(rule, selector, hash) {
     return [rule.parent.type, (rule.parent.params || 'root'), selector, hash || ''].join('-');
+}
+
+function isClass(c) {
+    return c.charAt(0) === '.';
 }
 
 function makeDeclsIntoArray(rule) {
@@ -19,11 +24,23 @@ function makeDeclsIntoArray(rule) {
     return decls;
 }
 
-function assertOnDecls(mapped, rule) {
+function assertOnDecls(mapped, rule, vals) {
     var mappedDecl = makeDeclsIntoArray(mapped[0].rule);
     var ruleDecl = makeDeclsIntoArray(rule);
+    var intersect = _.intersection(mappedDecl, ruleDecl);
 
-    return _.intersection(mappedDecl, ruleDecl);
+    if (vals) {
+        var defs = [];
+        rule.eachDecl(function (decl) {
+            defs.push([decl.prop, decl.value]);
+        });
+
+        return defs.filter(function (def) {
+            return intersect.indexOf(def[0]) > -1;
+        });
+    }
+
+    return intersect;
 }
 
 module.exports = function (filePath, opts) {
@@ -31,19 +48,30 @@ module.exports = function (filePath, opts) {
         strict: false
     }, opts);
 
-    return new Promise(function (res, rej) {
+    return new Promise(function (res) {
         var root = postcss.parse(fs.readFileSync(filePath));
         var map = {};
 
         root.eachRule(function (rule) {
             rule.selectors.forEach(function (selector) {
+                if (!isClass(selector)) {
+                    return;
+                }
+
                 var id = makeId(rule, selector);
 
                 if (map[id] && (options.strict ? assertOnDecls(map[id], rule).length : true)) {
-                    return map[id].push({
+                    var overWrite = {
                         selector: selector,
                         rule: rule
-                    });
+                    };
+
+                    if (options.strict) {
+                        overWrite.rulesOverwritten = assertOnDecls(map[id], rule, true);
+                    }
+
+                    map[id].push(overWrite);
+                    return;
                 }
 
                 map[id] = [{
@@ -62,12 +90,15 @@ module.exports = function (filePath, opts) {
         stuff.toString = function () {
             var str = '';
             this.forEach(function (selector) {
-                var first = selector.rules.shift();
+                var first = selector.shift();
                 str += '\n\n';
-                str += first.selector + ' was defined ' + first.rule.source.start.line + ':' + first.rule.source.start.column + '\n';
+                str += colors.green(first.selector) + colors.bold(' defined line ' + first.rule.source.start.line + ':' + first.rule.source.start.column) + ' ' + ' \n';
 
-                selector.rules.forEach(function (sel) {
-                    str += sel.selector + ' was mutated ' + sel.rule.source.start.line + ':' + sel.rule.source.start.column + '\n';
+                selector.forEach(function (sel) {
+                    str += colors.bold.grey('mutated line ' + sel.rule.source.start.line + ':' + sel.rule.source.start.column) + ' ' + colors.red((sel.rulesOverwritten || []).reduce(function (data, over) {
+                        data += over[0] + ': ' + over[1] + ' ';
+                        return data;
+                    }, '')) + '\n';
                 });
             });
 
